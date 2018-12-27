5c02f9aca6a713e7e07a7247f59e8cb717e25f4f
nenadprodanovic <nenadprodanovic@protech.rs>
2018-12-27 12:32:09 +0100

    test

---
+++ app/src/main/java/com/protech/utftest/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.protech.utftest
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val a = "\u00C4";
+        Log.e("TEST", a)
+    }
+}
